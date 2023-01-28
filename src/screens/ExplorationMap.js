a16cf5d72b45f9ad861aa77f4abb9401d1718b7d
NuoWenLei <nuowen0612@gmail.com>
2023-01-28 16:38:55 -0500

    add map uses

--- src/screens/ExplorationMap.js
+++ src/screens/ExplorationMap.js
@@ -1,28 +1,37 @@
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
+import thing from "../../assets/favicon.png";
 
 export default function ExplorationMap() {
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Hello world!</Text>
   <MapView
-            provider={PROVIDER_GOOGLE}
-            style={{flex:1, width: "100%", height: "100%"}}
-          />
+            style={{width: "100%", height: "100%"}}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+              }}
+          >
+          <Marker
+          coordinate = {{latitude: 37.78825,longitude: -122.4324}}
+          image={thing}/></MapView>
 </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
   
 
   
